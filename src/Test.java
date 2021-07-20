e832cb5dc35056852602c405433dcc9efa78dcfa
kimjusung <dlrtls12345@naver.com>
2021-07-20 16:07:02 +0900

    Q16234

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,8 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Test {
-
     public static void main(String[] args) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        dq.add(0);
-        dq.add(1);
-        dq.add(2);
-        dq.add(3);
-        System.out.print(dq.pollLast());
-        System.out.print(dq.size());
+        int[] a = {1, 2, 3};
+        int[] b = a;
+        a = new int[5];
+        System.out.print(b[0]);
     }
 }
