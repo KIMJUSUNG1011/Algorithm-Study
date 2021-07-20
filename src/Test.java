892d52df66574f1b7ed5919553f698d1794e1670
kimjusung <dlrtls12345@naver.com>
2021-07-21 01:29:27 +0900

    update

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,4 @@
 public class Test {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3};
-        int[] b = a;
-        a = new int[5];
-        System.out.print(b[0]);
     }
 }
