1c4aaf5f6726d7666c3612dc6c255f3f2886bc16
kimjusung <dlrtls12345@naver.com>
2021-08-10 10:36:24 +0900

    Q17822

--- src/Q17822.java
+++ src/Q17822.java
@@ -4,6 +4,9 @@ import java.util.*;
 public class Q17822 {
     static int N, M, T;
     static int[][] board = new int[50][50];
+    static int[][] visited = new int[50][50];
+    static int[][] delta = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    static boolean flag = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +20,7 @@ public class Q17822 {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());
@@ -27,6 +31,7 @@ public class Q17822 {
             }
             remove();
         }
+
         int answer = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -39,32 +44,23 @@ public class Q17822 {
     }
 
     static void remove() {
-        int[][] tmp_board = new int[N][M];
-        boolean flag = true;
-        int sum = 0, cnt = 0;
+        int cnt = 0, sum = 0;
+        flag = true;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (tmp_board[i][j] != -1) {
-                    tmp_board[i][j] = board[i][j];
-                }
-                if (board[i][j] != -1) {
-                    sum += board[i][j];
+                visited[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (visited[i][j] == 0 && board[i][j] != -1) {
+                    sum += dfs(i, j, board[i][j], 0);
                     cnt++;
-                    int tmp = board[i][j];
-                    int[][] delta = {{i, (j + 1) % M}, {i, (j + M - 1) % M}, {i - 1, j}, {i + 1, j}};
-                    for (int[] p : delta) {
-                        if (p[0] >= 0 && p[1] >= 0 && p[0] < N && p[1] < M) {
-                            if (tmp == board[p[0]][p[1]]) {
-                                tmp_board[p[0]][p[1]] = -1;
-                                tmp_board[i][j] = -1;
-                                flag = false;
-                            }
-                        }
-                    }
                 }
             }
         }
 
+        // 삭제된 수가 없음
         if (flag) {
             Double avg = (double)sum / cnt;
             for (int i = 0; i < N; i++) {
@@ -74,9 +70,35 @@ public class Q17822 {
                     }
                 }
             }
-        } else {
-            board = tmp_board;
         }
+    }
+
+    static int dfs(int r, int c, int val, int depth) {
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + delta[i][0];
+            int nc = c + delta[i][1];
+            if (nr >= 0 && nr < N) {
+                if (nc == -1) {
+                    nc = M - 1;
+                }
+                if (nc == M) {
+                    nc = 0;
+                }
+                if (visited[nr][nc] == 0 && board[nr][nc] == val) {
+                    visited[nr][nc] = 1;
+                    board[nr][nc] = -1;
+                    sum += dfs(nr, nc, val, ++depth);
+                    flag = false;
+                }
+            }
+        }
+
+        if (depth == 0 && sum != 0) {
+            board[r][c] = -1;
+        }
+
+        return val + sum;
     }
 
     static void rotate(int num, int d, int k) {
