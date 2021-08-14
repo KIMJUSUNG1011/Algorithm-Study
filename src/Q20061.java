ecc3652664bf94e6b3bd55f3bd03f363d9c20792
kimjusung <dlrtls12345@naver.com>
2021-08-15 03:24:51 +0900

    Q20061 processing

--- src/Q20061.java
+++ src/Q20061.java
@@ -3,8 +3,11 @@ import java.util.*;
 
 public class Q20061 {
     static int N;
+    static int score = 0;
     static int[][] green = new int[6][4];
+    static int[][] blue = new int[4][6];
     static int[] green_h = {0, 0, 0, 0};
+    static int[] blue_h = {0, 0, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,18 +17,109 @@ public class Q20061 {
             int t = Integer.parseInt(st.nextToken());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            green_process(t, x, y);
-            print(green);
+            green_process(t, y);
+            blue_process(t, x);
+        }
 
-            System.out.print("h : ");
-            for (int h : green_h) {
-                System.out.print(h + " ");
-            } System.out.println();
-            System.out.println();
+        int cnt = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (green[i][j] == 1) {
+                    cnt++;
+                }
+                if (blue[j][i] == 1) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(score);
+        System.out.print(cnt);
+    }
+
+    static void blue_process(int t, int x) {
+
+        if (t == 1) {
+            int h = blue_h[x];
+            blue[x][5 - h] = 1;
+            blue_h[x] = h + 1;
+        }
+
+        if (t == 2) {
+            int h = blue_h[x];
+            blue[x][5 - h] = 1;
+            blue[x][(5 - h) - 1] = 1;
+            blue_h[x] = h + 2;
+        }
+
+        if (t == 3) {
+            int h1 = blue_h[x], h2 = blue_h[x + 1];
+            int h = Math.max(h1, h2);
+            blue[x][5 - h] = 1;
+            blue[x + 1][5 - h] = 1;
+            blue_h[x] = h + 1;
+            blue_h[x + 1] = h + 1;
+        }
+
+        for (int i = 5; i >= 0; i--) {
+            boolean isRemove = true;
+            for (int j = 0; j < 4; j++) {
+                if (blue[j][i] == 0) {
+                    isRemove = false;
+                    break;
+                }
+            }
+
+            // i 열 삭제
+            if (isRemove) {
+                score++;
+                for (int j = 0; j < 4; j++) {
+                    int h = blue_h[j];
+                    blue[j][i] = 0;
+                    blue_h[j] = 0;
+
+                    // blue 배열 재설정
+                    for (int k = i - 1; k >= 6 - h; k--) {
+                        blue[j][k + 1] = blue[j][k];
+                        blue[j][k] = 0;
+                    }
+
+                    // blue_h 배열 재설정
+                    for (int k = 0; k <= 5; k++) {
+                        if (blue[j][k] == 1) {
+                            blue_h[j] = 6 - k;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // 0, 1 번 열에 블록이 있는 경우
+        int cnt = 0;
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (blue[j][i] == 1) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        if (cnt > 0) {
+            for (int i = 5 - cnt; i >= 0; i--) {
+                for (int j = 0; j < 4; j++) {
+                    blue[j][i + cnt] = blue[j][i];
+                    blue[j][i] = 0;
+                }
+            }
+            for (int i = 0; i < 4; i++) {
+                blue_h[i] = blue_h[i] <= cnt ? 0 : blue_h[i] - cnt;
+            }
         }
     }
 
-    static void green_process(int t, int x, int y) {
+    static void green_process(int t, int y) {
 
         if (t == 1) {
             int h = green_h[y];
@@ -60,6 +154,7 @@ public class Q20061 {
 
             // i 행 삭제
             if (isRemove) {
+                score++;
                 for (int j = 0; j < 4; j++) {
                     int h = green_h[j];
                     green[i][j] = 0;
@@ -100,14 +195,30 @@ public class Q20061 {
                     green[i][j] = 0;
                 }
             }
+            for (int i = 0; i < 4; i++) {
+                green_h[i] = green_h[i] <= cnt ? 0 : green_h[i] - cnt;
+            }
         }
     }
 
     static void print(int[][] arr) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(arr[i][j]+ " ");
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             } System.out.println();
-        }
+        } System.out.println();
     }
 }
+
+/*
+        9
+        2 1 0
+        2 1 0
+        2 1 0
+        2 1 0
+        2 1 0
+        3 0 2
+        3 0 2
+        3 0 3
+        3 0 3
+ */
