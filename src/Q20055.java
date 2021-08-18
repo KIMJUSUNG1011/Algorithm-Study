76875e59f62c1c3283f2afd733132db851050a46
kimjusung <dlrtls12345@naver.com>
2021-08-18 18:20:55 +0900

    Q20555

--- src/Q20055.java
+++ src/Q20055.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Q20055 {
     static int N, K;
-    static int[] belt = new int[200];
-    static boolean[] visited = new boolean[100];
-    static Queue<Integer> q = new LinkedList<>();
+    static Board[] belt = new Board[200];
+    static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,31 +13,15 @@ public class Q20055 {
         K = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N * 2; i++) {
-            belt[i] = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            belt[i] = new Board(a, 0);
         }
 
-        int stage = 1;
-        while (true) {
+        int stage = 0;
+        while (cnt < K) {
             rotate();
             move();
-
-            // 올리는 위치에 로봇을 올림
-            if (!visited[0] && belt[0] > 0) {
-                visited[0] = true;
-                belt[0]--;
-                q.add(0);
-            }
-
-            int cnt = 0;
-            for (int i = 0; i < N * 2; i++) {
-                if (belt[i] == 0) {
-                    cnt++;
-                }
-            }
-            if (cnt >= K) {
-                break;
-            }
-
+            addRobot();
             stage++;
         }
 
@@ -46,48 +29,55 @@ public class Q20055 {
     }
 
     static void rotate() {
-        // 내구도 회전
-        int tmp = belt[N * 2 - 1];
+        // 벨트, 로봇 회전
+        Board tmp = belt[N * 2 - 1];
         for (int i = N * 2 - 2; i >= 0; i--) {
             belt[i + 1] = belt[i];
-        } belt[0] = tmp;
-
-        // 로봇 회전
-        int size = q.size();
-        for (int i = 0; i < size; i++) {
-            int x = q.remove();
-            visited[x] = false;
-            visited[x + 1] = true;
 
             // 로봇이 내리는 위치에 도달한 경우 즉시 내림
-            if (x + 1 == N - 1) {
-                visited[x + 1] = false;
-                continue;
+            if (i + 1 == N - 1) {
+                belt[i + 1].robot = 0;
             }
-
-            q.add(x + 1);
-        }
+        } belt[0] = tmp;
     }
 
     static void move() {
-        int size = q.size();
-        for (int i = 0; i < size; i++) {
-            int x = q.remove();
-
-            // 로봇을 한 칸 이동
-            if (!visited[x + 1] && belt[x + 1] >= 1) {
-                belt[x + 1]--;
-                visited[x + 1] = true;
-                visited[x] = false;
-                x++;
-
-                // 로봇이 내리는 위치에 도달한 경우 즉시 내림
-                if (x == N - 1) {
-                    visited[x] = false;
+        for (int i = N - 1; i >= 0; i--) {
+            if (belt[i].robot == 1) {
+                // 이동하려는 칸에 로봇이 있거나 내구도가 부족한 경우
+                if (belt[i + 1].robot == 1 || belt[i + 1].dur < 1) {
                     continue;
                 }
+                belt[i].robot = 0;
+                belt[i + 1].dur--;
+                if (belt[i + 1].dur == 0) {
+                    cnt++;
+                }
+
+                // 로봇이 내리는 위치에 도달한 것이 아닌 경우
+                if (i + 1 != N - 1) {
+                    belt[i + 1].robot = 1;
+                }
             }
-            q.add(x);
+        }
+    }
+
+    static void addRobot() {
+        // 올리는 위치에 로봇을 올림
+        if (belt[0].robot == 0 && belt[0].dur > 0) {
+            belt[0].robot = 1;
+            belt[0].dur--;
+            if (belt[0].dur == 0) {
+                cnt++;
+            }
+        }
+    }
+
+    static class Board {
+        int dur, robot;
+        public Board(int dur, int robot) {
+            this.dur = dur;
+            this.robot = robot;
         }
     }
 }
