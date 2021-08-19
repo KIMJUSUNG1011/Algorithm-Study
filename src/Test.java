2074264043435921e9a3c4b2005469b53072e342
kimjusung <dlrtls12345@naver.com>
2021-08-19 19:31:31 +0900

    Q20057

--- src/Test.java
+++ src/Test.java
@@ -1,44 +1,9 @@
 import java.util.*;
 
 public class Test {
-    static int N = 4;
-    static int[][] delta = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    static int[][] visited = new int[4][4];
-    static int[][] map = {
-            {1, 1, 1, 1},
-            {1, 2, 1, 1},
-            {1, 2, 2, 2},
-            {2, 1, 1, 2}
-    };
 
     public static void main(String[] args) {
-        int sum = dfs(0, 0, map[0][0]);
-        System.out.println(sum);
-        printMap();
-    }
-
-    static int dfs(int r, int c, int val) {
-        visited[r][c] = 1;
-        int sum = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int nr = r + delta[i][0];
-            int nc = c + delta[i][1];
-            if ((nr >= 0 && nc >= 0 && nr < N && nc < N) && visited[nr][nc] == 0) {
-                if (map[nr][nc] == val) {
-                    sum += dfs(nr, nc, val);
-                }
-            }
-        }
-
-        return val + sum;
-    }
-
-    static void printMap() {
-        for (int i = 0 ; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(visited[i][j] + " ");
-            } System.out.println();
-        }
+        double d = 0.05;
+        System.out.println(d == 0.04);
     }
 }
