273ee7ab27e812f5525f7e6849ef973c5670101e
kimjusung <dlrtls12345@naver.com>
2021-08-21 17:27:29 +0900

    Q20058 processing

--- src/Q20058.java
+++ src/Q20058.java
@@ -20,11 +20,39 @@ public class Q20058 {
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < Q; i++) {
             int L = Integer.parseInt(st.nextToken());
-            rotate(L);
+            solve(L);
         }
     }
 
-    static void rotate(int L) {
+    static void solve(int L) {
+        System.out.println(L);
+        int partLen = (int)Math.pow(2, L);
+        int[][] tmpBoard = new int[len][len];
+        for (int i = 0; i <= len - 1; i += partLen) {
+            for (int j = 0; j <= len - 1; j += partLen) {
+                rotate(i, j, partLen, tmpBoard);
+            }
+        }
+        printBoard(tmpBoard);
+    }
 
+    static void rotate(int r, int c, int partLen, int[][] tmp) {
+        int s1 = r + (c + partLen - 1);
+        int s2 = r - c;
+        System.out.println(partLen);
+        System.out.println(r + "," + c + " / " + s1 + "," + s2);
+        for (int i = r; i < r + partLen; i++) {
+            for (int j = c; j < c + partLen; j++) {
+                tmp[s2 + j][s1 - i] = board[i][j];
+            }
+        }
+    }
+
+    static void printBoard(int[][] board) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                System.out.print(board[i][j] + " ");
+            } System.out.println();
+        } System.out.println();
     }
 }
