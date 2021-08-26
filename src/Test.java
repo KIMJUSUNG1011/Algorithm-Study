876ab35346e9fb255d6caf393f39e202adb1f95f
kimjusung <dlrtls12345@naver.com>
2021-08-26 15:05:51 +0900

    Q14888

--- src/Test.java
+++ src/Test.java
@@ -1,35 +1,25 @@
 import java.util.*;
 
 public class Test {
-    static int[][] tmp_delta = {{0 ,1}, {1, 0}};
-    static int[][] visited = new int[20][20];
-    static int N = 10;
+    static int[] arr = {0, 0, 2, 3};
+    static boolean[] visited = new boolean[4];
 
     public static void main(String[] args) {
-        int[][] tmp_board = new int[10][10];
-        Queue<int[]> q = new LinkedList<>();
-        visited[2][3] = 1;
-        q.add(new int[]{2, 3});
-        while (!q.isEmpty()) {
-            int[] p = q.poll();
-            for (int i = 0; i < 2; i++) {
-                int nr = p[0] + tmp_delta[i][0];
-                int nc = p[1] + tmp_delta[i][1];
-                if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-                    continue;
-                }
-                if (visited[nr][nc] == 0) {
-                    visited[nr][nc] = 1;
-                    q.add(new int[]{nr, nc});
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(visited[i][j] + " ");
-            } System.out.println();
-        }
+        go(0);
     }
 
+    static void go(int index) {
+
+        if (index == arr.length) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+           if (!visited[i]) {
+               visited[i] = true;
+               go(index + 1);
+               visited[i] = false;
+           }
+        }
+    }
 }
