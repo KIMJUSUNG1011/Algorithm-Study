251b20287409e86311ad13ab3a168c515218db58
kimjusung <dlrtls12345@naver.com>
2021-09-05 16:33:42 +0900

    last processing..

--- src/Q17070.java
+++ src/Q17070.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Q16637 {
+public class Q17070 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/Q17406.java
+++ src/Q17406.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Q16637 {
+public class Q17406 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
