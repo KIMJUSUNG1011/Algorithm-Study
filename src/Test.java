aed73eaab53c60a2c8e7c628b15df15f910abfc3
kimjusung <dlrtls12345@naver.com>
2021-09-10 16:17:46 +0900

    delete

--- src/Test.java
+++ src/Test.java
@@ -1,52 +1,34 @@
 import java.util.*;
 
 public class Test {
-    static int[] arr = {0, 1, 2, 3};
-    static int[] order = new int[2];
-
     public static void main(String[] args) {
-        go(0, 0);
-    }
+        double[] arr = {
+                2.6, 1.8, 2.6, 3.7, 1.9, 4.2, 3.8, 2.1, 2.7, 3.0,
+                4.0, 2.4, 2.3, 4.3, 3.1, 2.6, 2.6, 2.5, 2.7, 2.7,
+                2.9, 3.4, 1.9, 2.3, 3.3, 2.2, 3.5, 3.0, 2.5, 3.4
+        };
 
-    static void go(int index, int cnt) {
-
-        if (cnt == 2) {
-            for (int o : order) {
-                System.out.print(o + " ");
-            } System.out.println();
-            return;
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
+        System.out.println("sum : " + sum);
+        System.out.println("avg : " + (sum / arr.length));
+        Arrays.sort(arr);
 
-        if (index == 4) {
-            return;
-        }
-
-        order[cnt] = index;
-        go(index + 1, cnt + 1);
-        order[cnt] = 0;
-
-        go(index + 1, cnt);
-    }
-
-    /*
-    static void go(int index, int start) {
-
-        if (index == 2) {
-            for (int o : order) {
-                System.out.print(o + " ");
-            } System.out.println();
-            return;
-        }
-
-        for (int i = start; i < 4; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                order[index] = arr[i];
-                go(index + 1, i + 1);
-                order[index] = 0;
-                visited[i] = false;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
             }
         }
+
+        int idx1 = ((arr.length / 2) - 1) / 2;
+        int idx2 = ((arr.length / 2) + (arr.length - 1)) / 2;
+        System.out.println("Q1 : " + arr[idx1]);
+        System.out.println("Q2 : " + arr[idx2]);
+
+        System.out.println("max : " + arr[arr.length - 1]);
+        System.out.println("min : " + arr[0]);
     }
-     */
 }
