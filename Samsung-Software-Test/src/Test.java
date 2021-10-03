5bc0c5dd1210ab5b4ed071f90eeb9b38a5ec8b43
김주성 <dlrtls12345@naver.com>
2021-10-04 01:00:56 +0900

    backup commit

--- Samsung-Software-Test/src/Test.java
+++ Samsung-Software-Test/src/Test.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class Test {
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) {
         two(10);
         System.out.println(sb);
     }
+
     static void two(int n) {
         if (n == 1) {
             sb.append(n);
