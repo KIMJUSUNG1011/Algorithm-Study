b4afffd32efade51c625c06d6a3818c759a36925
김주성 <dlrtls12345@naver.com>
2022-04-21 00:41:43 +0900

    Q17825_2

--- Samsung-Software-Test/src/Q17825_2.java
+++ Samsung-Software-Test/src/Q17825_2.java
@@ -28,16 +28,18 @@ public class Q17825_2 {
 
         if (index == 10) {
             answer = Math.max(answer, sum);
-            if (sum >= 190) {
-                System.out.println(sum);
-            }
             return;
         }
 
         for (int i = 0; i < 4; i++) {
 
-            int val = 0;
             int[] h = horse[i];
+
+            if (h[1] > 40) {
+                continue;
+            }
+
+            int val = 0;
             int[] prev = {h[0], h[1]};
             int[] next = roll(h, dice[index]);
 
@@ -61,6 +63,36 @@ public class Q17825_2 {
             horse[i][1] = prev[1];
             horse[i][0] = prev[0];
         }
+    }
+
+    static int[] roll(int[] k, int val) {
+
+        int route = k[0], pos = k[1];
+
+        if (route == 0 && pos % 10 == 0) {
+            route = pos / 10;
+        }
+
+        for (int i = 0; i < val; i++) {
+
+            pos = map[route][pos];
+
+            // 도착칸 도달
+            if (pos > 40) {
+                break;
+            }
+
+            if (pos == 25 || pos == 40) {
+                route = 4;
+            }
+        }
+
+        // 이동을 마치는 칸에 다른 말이 있는 경우
+        if (pos <= 40 && check[route][pos] == 1) {
+            return null;
+        }
+
+        return new int[]{route, pos};
     }
 
     static void init() {
@@ -92,35 +124,5 @@ public class Q17825_2 {
         map[4][30] = 35;
         map[4][35] = 40;
         map[4][40] = 42;
-    }
-
-    static int[] roll(int[] k, int val) {
-
-        int route = k[0], pos = k[1];
-
-        if (pos % 10 == 0) {
-            route = pos / 10;
-        }
-
-        for (int i = 0; i < val; i++) {
-
-            pos = map[route][pos];
-
-            // 도착칸 도달
-            if (pos > 40) {
-                break;
-            }
-
-            if (pos == 25 || pos == 40) {
-                route = 4;
-            }
-        }
-
-        // 이동을 마치는 칸에 다른 말이 있는 경우
-        if (pos <= 40 && check[route][pos] == 1) {
-            return null;
-        }
-
-        return new int[]{route, pos};
     }
 }
