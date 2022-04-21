1460879e1d4f4b0b80e3f247b544a3097e52199d
김주성 <dlrtls12345@naver.com>
2022-04-21 17:19:31 +0900

    Q20061_2

--- Samsung-Software-Test/src/Q20061_2.java
+++ Samsung-Software-Test/src/Q20061_2.java
@@ -3,9 +3,10 @@ import java.util.*;
 
 public class Q20061_2 {
 
-    static int N;
-    static int[][] greenMap = new int[6][4];
-    static int[] greenRow = {5, 5, 5, 5};
+    static int N, score = 0, cnt = 0;
+    static int[][][] map = new int[2][6][4];
+    static int[][] height = {{5, 5, 5, 5}, {5, 5, 5, 5}};
+    static int[] convert = {0, 1, 3, 2};
 
     public static void main(String[] args) throws IOException {
 
@@ -17,48 +18,65 @@ public class Q20061_2 {
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            move(t, c);
+            // 초록색 보드
+            move(t, c, 0);
 
-            print(greenMap, greenRow);
-
-            System.out.println();
+            // 파란색 보드
+            move(convert[t], 3 - r, 1);
         }
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0 ; j < 4; j++) {
+                if (map[0][i][j] == 1) {
+                    cnt++;
+                }
+                if (map[1][i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.print(score + "\n" + cnt);
     }
 
-    static void move(int t, int firstCol) {
+    static void move(int t, int firstCol, int color) {
 
         int inputRow;
 
+        if (color == 1 && t == 2) {
+            firstCol--;
+        }
+
         if (t == 1) {
-            inputRow = greenRow[firstCol];
-            greenMap[inputRow][firstCol] = 1;
-            greenRow[firstCol]--;
+            inputRow = height[color][firstCol];
+            map[color][inputRow][firstCol] = 1;
+            height[color][firstCol]--;
         }
 
         if (t == 2) {
-            inputRow = Math.min(greenRow[firstCol], greenRow[firstCol + 1]);
-            greenMap[inputRow][firstCol] = greenMap[inputRow][firstCol + 1] = 1;
-            greenRow[firstCol] = greenRow[firstCol + 1] = inputRow - 1;
+            inputRow = Math.min(height[color][firstCol], height[color][firstCol + 1]);
+            map[color][inputRow][firstCol] = map[color][inputRow][firstCol + 1] = 1;
+            height[color][firstCol] = height[color][firstCol + 1] = inputRow - 1;
         }
 
         if (t == 3) {
-            inputRow = greenRow[firstCol];
-            greenMap[inputRow][firstCol] = greenMap[inputRow - 1][firstCol] = 1;
-            greenRow[firstCol] -= 2;
+            inputRow = height[color][firstCol];
+            map[color][inputRow][firstCol] = map[color][inputRow - 1][firstCol] = 1;
+            height[color][firstCol] -= 2;
         }
 
-        delete();
+        delete(color);
     }
 
-    static void delete() {
+    static void delete(int color) {
 
         int newRow = 5, minRow = 5;
-        int[][] tmpMap = new int[6][4];
+        int[][][] tmpMap = new int[2][6][4];
         int[] deleteRow = {1, 1, 1, 1, 1, 1};
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 4; j++) {
-                if (greenMap[i][j] == 0) {
+                if (map[color][i][j] == 0) {
                     deleteRow[i] = 0;
                     break;
                 }
@@ -68,21 +86,23 @@ public class Q20061_2 {
         for (int i = 5; i >= 0; i--) {
             if (deleteRow[i] == 0) {
                 for (int j = 0; j < 4; j++) {
-                    tmpMap[newRow][j] = greenMap[i][j];
+                    tmpMap[color][newRow][j] = map[color][i][j];
                 }
                 newRow--;
+            } else {
+                score++;
             }
         }
 
         for (int i = 0; i < 4; i++) {
-            greenRow[i] = 5;
+            height[color][i] = 5;
             for (int j = 0; j < 6; j++) {
-                if (tmpMap[j][i] == 1) {
-                    greenRow[i] = j - 1;
+                if (tmpMap[color][j][i] == 1) {
+                    height[color][i] = j - 1;
                     break;
                 }
             }
-            minRow = Math.min(minRow, greenRow[i]);
+            minRow = Math.min(minRow, height[color][i]);
         }
 
         // 연한 부분 삭제
@@ -92,17 +112,20 @@ public class Q20061_2 {
 
             for (int i = 5 - nDelete; i >= 0; i--) {
                 for (int j = 0; j < 4; j++) {
-                    tmpMap[i + nDelete][j] = tmpMap[i][j];
-                    tmpMap[i][j] = 0;
+                    tmpMap[color][i + nDelete][j] = tmpMap[color][i][j];
+                    tmpMap[color][i][j] = 0;
                 }
             }
 
             for (int i = 0; i < 4; i++) {
-                greenRow[i] += nDelete;
+                height[color][i] += nDelete;
+                if (height[color][i] > 5) {
+                    height[color][i] = 5;
+                }
             }
         }
 
-        greenMap = tmpMap;
+        map[color] = tmpMap[color];
     }
 
     static void print(int[][] map, int[] row) {
