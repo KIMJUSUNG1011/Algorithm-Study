a99e69b62e467cd8f68ecefdc77f32cde90edd39
김주성 <dlrtls12345@naver.com>
2022-04-23 02:24:16 +0900

    Q17136_2 v2

--- Samsung-Software-Test/src/Q17136_2.java
+++ Samsung-Software-Test/src/Q17136_2.java
@@ -21,17 +21,6 @@ public class Q17136_2 {
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = 1; k <= 5; k++) {
-                    if (check(i, j, k) == -1) {
-                        break;
-                    }
-                    maxPaper[i][j] = k;
-                }
-            }
-        }
-
         dfs(0, 0, 0);
 
         if (answer == 25) {
@@ -43,6 +32,10 @@ public class Q17136_2 {
 
     static void dfs(int index, int sum, int cnt) {
 
+        while (index < 100 && map[index / 10][index % 10] != 1) {
+            index++;
+        }
+
         int r = index / 10;
         int c = index % 10;
 
@@ -53,14 +46,10 @@ public class Q17136_2 {
             return;
         }
 
-        if (map[r][c] == 0 || map[r][c] == 2) {
-            dfs(index + 1, sum, cnt);
-            return;
-        }
-
-        int maxLen = maxPaper[r][c];
-
-        for (int i = 1; i <= maxLen; i++) {
+        for (int i = 1; i <= 5; i++) {
+            if (check(r, c, i) == -1) {
+                break;
+            }
             if (paper[i] > 0) {
                 paper[i]--;
                 go(r, c, i, 0);
@@ -72,18 +61,8 @@ public class Q17136_2 {
     }
 
     static int check(int r, int c, int len) {
-        for (int i = r; i < r + len; i++) {
-            for (int j = c; j < c + len; j++) {
-                if ((i < 0 || j < 0 || i >= 10 || j >= 10) || map[i][j] == 0) {
-                    return -1;
-                }
-            }
-        }
-        return 0;
-    }
 
-    /*
-    static int check(int r, int c, int len) {
+        /*
         for (int i = r; i < r + len; i++) {
             for (int j = c; j < c + len; j++) {
                 if (i < 0 || j < 0 || i >= 10 || j >= 10) {
@@ -94,10 +73,9 @@ public class Q17136_2 {
                 }
             }
         }
+         */
         return 0;
     }
-
-     */
 
     static void go(int r, int c, int len, int type) {
         for (int i = r; i < r + len; i++) {
