bd3b89c8894aac2463ebe549d3fabf1882ead7ad
kimjusung <dlrtls12345@naver.com>
2022-05-10 10:31:22 +0900

    test

--- Samsung-Software-Test/src/Test.java
+++ Samsung-Software-Test/src/Test.java
@@ -1,34 +1,6 @@
-import java.util.*;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        Queue<A> q1 = new LinkedList<>();
-        Queue<A> q2 = new LinkedList<>();
-
-        for (int i = 1; i <= 5; i++) {
-            q2.add(new A(i));
-        }
-
-        q1.addAll(q2);
-
-        for (int i = 0; i < q2.size(); i++) {
-            A a = q2.poll();
-            a.a = 10;
-            q2.add(a);
-        }
-
-        while (!q1.isEmpty()) {
-            System.out.println(q1.poll().a);
-        }
-
-    }
-
-    static class A {
-        int a;
-        public A(int a) {
-            this.a = a;
-        }
     }
 }
